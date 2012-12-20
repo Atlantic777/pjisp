d1bf5737521f617c81485e5900b959f616c4fd6d
Atlantic777 <atlantic777@lugons.org>
2012-12-20 19:31:23 +0100

    Loading polinoms.

--- Priprema za proveru 11/polinom/poli.c
+++ Priprema za proveru 11/polinom/poli.c
@@ -1,17 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int** init_poly(int);
+void load_poly(int**, int, FILE*);
+void print_poly(int**, int);
+
 int main()
 {
-	FILE *ifile = fopen("input.txt", "r");
-	int m, i = 1;
-	char n;
-	
-	do {
-		fscanf(ifile, "%d%c", &m, &n);
-		printf("%d %d\n", i, m);
-		i++;
-	} while(n != '\n');
-	
+	FILE *in = fopen("input.txt", "r");
+
+	int deg, i;
+	fscanf(in, "%d ", &deg);
+
+	int **poly_1 = init_poly(deg);
+	load_poly(poly_1, deg, in);
+	print_poly(poly_1, deg);
+
 	return 0;
+}
+
+int** init_poly(int deg)
+{
+	int i;
+	int **p = calloc(deg, sizeof(int*));
+
+	for(i = 0; i<deg; i++)
+		p[i] = calloc(2, sizeof(int));
+	
+	return p;
+}
+
+void load_poly(int** p, int deg, FILE* in)
+{
+	int i;
+	for(i = 0; i<deg; i++)
+	{
+		p[i][0] = i;
+		fscanf(in, "%d ", &p[i][0]);
+	}
+}
+
+void print_poly(int** p, int deg)
+{
+	int i;
+	for(i = 0; i<deg; i++)
+		printf("%dx^%d ", p[i][0], i);
+	puts("");
 }
